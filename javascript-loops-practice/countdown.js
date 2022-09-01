79f41d9c798d65fcf0be0be9a06ad29383d9ca10
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-01 16:55:54 +0000

    Completed javascript-loops-practice

--- javascript-loops-practice/countdown.js
+++ javascript-loops-practice/countdown.js
@@ -1,12 +1,12 @@
 /* exported countdown */
 
-// - create storage for output (array)
-// - start at the number
-// - look at numbers from number to 0
-//   - count down incrementally
-//   - check to make sure the number is not negative
-// - add the number to output
-// - give list of output
+// create storage for output (array)
+// start at the number
+// look at numbers from number to 0
+// count down incrementally
+// check to make sure the number is not negative
+// add the number to output
+// give list of output
 
 function countdown(number) {
   var output = [];
