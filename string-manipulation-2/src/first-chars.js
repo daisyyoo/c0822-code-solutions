350337ee66c095c32a06cf3cdb46c055e30e182f
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-07 03:52:04 +0000

    Completed string-manipulation-2

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,19 @@
 /* exported firstChars */
+
+// make a new variable with an empty string
+// if the length given is less than the string.length,
+// add the values, starting with the first and looped until the length provided
+// if length is more than the string.length, return string
+// output the variable that started with empty variable
+
+function firstChars(length, string) {
+  var newString = '';
+  for (var i = 0; i < length; i++) {
+    if (length < string.length) {
+      newString += string[i];
+    } else {
+      return string;
+    }
+  }
+  return newString;
+}
