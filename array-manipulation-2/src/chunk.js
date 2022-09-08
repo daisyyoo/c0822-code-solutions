e28497c82668314f2879402dd9cf80523705254a
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-08 22:35:26 +0000

    working on array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,5 +1,11 @@
 /* exported chunk */
 
+// make 2 for loops
+// first one to increment per given size
+// make new variable with empty array
+// second loop to increment +1 to size, using to see if the value at size has value
+// if no value, then return to first loop and then make sub array again
+
 // make the outside array container
 // make smaller array container to fit size
 // loop through array starting at 0, and add value into smaller array
@@ -11,35 +17,35 @@
 // add all the smaller arrays into outside array container
 // return outside array container
 
-function chunk(array, size) {
-  var bigArray = [];
-  var array1 = [];
-  var array2 = [];
-  var array3 = [];
-  var array4 = [];
-  var array5 = [];
+// function chunk(array, size) {
+//   var bigArray = [];
+//   var array1 = [];
+//   var array2 = [];
+//   var array3 = [];
+//   var array4 = [];
+//   var array5 = [];
 
-  for (var i = 0; i < array.length; i++) {
-    if (size > array.length) {
-      return array;
-    } else if (array1.length < size) {
-      array1.push(array[i]);
-    } else if (array1.length >= size && array2.length < size) {
-      array2.push(array[i]);
-    } else if (array2.length >= size && array3.length < size) {
-      array3.push(array[i]);
-    } else if (array3.length >= size && array4.length < size) {
-      array2.push(array[i]);
-    } else if (array4.length >= size && array5.length < size) {
-      array3.push(array[i]);
-    }
-  }
-  bigArray.push(array1);
-  bigArray.push(array2);
-  bigArray.push(array3);
-  bigArray.push(array4);
-  bigArray.push(array5);
+//   for (var i = 0; i < array.length; i++) {
+//     if (size > array.length) {
+//       return array;
+//     } else if (array1.length < size) {
+//       array1.push(array[i]);
+//     } else if (array1.length >= size && array2.length < size) {
+//       array2.push(array[i]);
+//     } else if (array2.length >= size && array3.length < size) {
+//       array3.push(array[i]);
+//     } else if (array3.length >= size && array4.length < size) {
+//       array2.push(array[i]);
+//     } else if (array4.length >= size && array5.length < size) {
+//       array3.push(array[i]);
+//     }
+//   }
+//   bigArray.push(array1);
+//   bigArray.push(array2);
+//   bigArray.push(array3);
+//   bigArray.push(array4);
+//   bigArray.push(array5);
 
-  // if they don't exist, or are not equal to empty array, then push into bigArray
-  return bigArray;
-}
+//   // if they don't exist, or are not equal to empty array, then push into bigArray
+//   return bigArray;
+// }
