11bdfa4aec1a6f09de16051a2dfe629836387881
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-08 19:19:39 +0000

    Completed javascript-modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,20 +1,20 @@
 var $openButton = document.querySelector('.open-button');
 var $noButton = document.querySelector('.no');
-var $backgroundClosed = document.querySelector('.dark-background-closed');
-var $surveyBoxClosed = document.querySelector('.survey-box-closed align-content');
 var survey = true;
 
 function fillSurvey(event) {
   event.preventDefault();
   if (survey === true) {
     var $backgroundOpen = document.querySelector('.dark-background');
-    var $surveyBoxOpen = document.querySelector('.survey-box align-content');
-    $backgroundOpen.className = '.dark-background-closed';
-    $surveyBoxOpen.className = '.survey-box-closed align-content';
+    var $surveyBoxOpen = document.querySelector('.survey-box');
+    $backgroundOpen.className = 'dark-background-closed';
+    $surveyBoxOpen.className = 'survey-box-closed align-content';
     survey = false;
   } else if (survey === false) {
-    $backgroundClosed.className = '.dark-background';
-    $surveyBoxClosed.className = '.survey-box align-content';
+    var $backgroundClosed = document.querySelector('.dark-background-closed');
+    var $surveyBoxClosed = document.querySelector('.survey-box-closed.align-content');
+    $backgroundClosed.className = 'dark-background';
+    $surveyBoxClosed.className = 'survey-box align-content';
     survey = true;
   }
 }
