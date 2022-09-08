a868fb77a86bd9bd0a97c6f35648e7f939fc377f
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-08 17:37:52 +0000

    Completed dom-event-delegation

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -12,3 +12,19 @@ function checked(event) {
     }
   }
 }
+
+// solution
+// var $taskList = document.querySelector('.task-list');
+// $taskList.addEventListener('click', function (event) {
+
+//   console.log('event.target: ', event.target);
+//   console.log('event.target.tagName: ', event.target.tagName);
+
+//   if (event.target.tagName !== 'BUTTON') {
+//   return;
+//   } //guard clause
+
+// var $taskListItem = event.target.closest('.task-list-item');
+// console.log('closest .task-list-item', $taskListItem);
+// $taskListItem.remove();
+// }
