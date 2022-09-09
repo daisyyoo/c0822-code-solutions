dc7a17f09a044f08584843fb50878d625c67ac2d
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-09 23:55:19 +0000

    working on object-manipulation-2

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,18 @@
 /* exported invert */
+
+// create a new object container
+// create a new variable to hold property, that'll become value
+// create a new variable to hold value to become property
+// use the for in to loop the data
+// assign it per loop and then add to new object container
+// output new container
+
+function invert(source) {
+  var newObject = {};
+  for (var key in source) {
+    var property = key;
+    var value = source[key];
+    newObject[value] = property;
+  }
+  return newObject;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,17 @@
 /* exported pick */
+
+// make new object container
+// Use a for loop to loop through the keys array
+// if it exists in keys array,
+// then add into object
+// return new object container
+
+function pick(source, keys) {
+  var newObject = {};
+  for (var i = 0; i < keys.length; i++) {
+    if (source[keys[i]] !== undefined) {
+      newObject[keys[i]] = source[keys[i]];
+    }
+  }
+  return newObject;
+}
