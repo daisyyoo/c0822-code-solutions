0569b1abc29aacc4970b30651ad8dfb819c57c64
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-09 04:50:44 +0000

    Completed array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,51 +1,26 @@
 /* exported chunk */
 
+// make an empty array for final output
 // make 2 for loops
 // first one to increment per given size
 // make new variable with empty array
-// second loop to increment +1 to size, using to see if the value at size has value
-// if no value, then return to first loop and then make sub array again
+// second loop to increment +1,
+// if sub array has reached size, then exit loop, reset array value and loop again
+// push the sub array every time the loop is exited
+// return the final array at the way end
 
-// make the outside array container
-// make smaller array container to fit size
-// loop through array starting at 0, and add value into smaller array
-// when length of smaller array reaches size,
-// make another smaller container to fit leftover
-// keep looping and adding into the other smaller container
-// until this one's length reaches size also
-// when end of original array is reached
-// add all the smaller arrays into outside array container
-// return outside array container
-
-// function chunk(array, size) {
-//   var bigArray = [];
-//   var array1 = [];
-//   var array2 = [];
-//   var array3 = [];
-//   var array4 = [];
-//   var array5 = [];
-
-//   for (var i = 0; i < array.length; i++) {
-//     if (size > array.length) {
-//       return array;
-//     } else if (array1.length < size) {
-//       array1.push(array[i]);
-//     } else if (array1.length >= size && array2.length < size) {
-//       array2.push(array[i]);
-//     } else if (array2.length >= size && array3.length < size) {
-//       array3.push(array[i]);
-//     } else if (array3.length >= size && array4.length < size) {
-//       array2.push(array[i]);
-//     } else if (array4.length >= size && array5.length < size) {
-//       array3.push(array[i]);
-//     }
-//   }
-//   bigArray.push(array1);
-//   bigArray.push(array2);
-//   bigArray.push(array3);
-//   bigArray.push(array4);
-//   bigArray.push(array5);
-
-//   // if they don't exist, or are not equal to empty array, then push into bigArray
-//   return bigArray;
-// }
+function chunk(array, size) {
+  var finalArray = [];
+  for (var i = 0; i < array.length; i += size) {
+    var newArray = [];
+    for (var j = i; j < array.length; j++) {
+      if (newArray.length !== size) {
+        newArray.push(array[j]);
+      } else {
+        break;
+      }
+    }
+    finalArray.push(newArray);
+  }
+  return finalArray;
+}
