857b491d059f2cf3ef7e4e177b6c2ff56601f5f8
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-09 03:03:20 +0000

    Completed javascript-typing-tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,27 +1,15 @@
 var $letter = document.querySelectorAll('.letter');
 var wholeDoc = document.querySelector('body');
+var counter = 0;
 
 function typeTest(event) {
-  for (var i = 0; i < $letter.length; i++) {
-    if (event.key === $letter[i].textContent) {
-      $letter[i].className = 'correct-letter';
-      $letter[i + 1].className = 'letter border';
-    }
+  if (event.key === $letter[counter].textContent) {
+    $letter[counter].className = 'correct-letter';
+    $letter[counter + 1].className = 'letter border';
+    counter++;
+  } else if (event.key !== $letter[counter].textContent) {
+    $letter[counter].className = 'incorrect-letter';
   }
-  // if (event.key !== $letter[i].textContent) {
-  //   $letter[i].className = 'incorrect-letter';
 }
 
 wholeDoc.addEventListener('keydown', typeTest);
-
-// when typed correctly, it stays green, border bottom disappears
-// the letter only has border bottom when at $letter[i]
-// when typed incorrectly, the border bottom also red
-
-// waiting for next letter is letter and border
-// classes correct-letter and incorrect-letter
-
-// PROBLEMS
-// everything else turns incorrect and red,
-// when it should only be when that letter is incorrect
-// if the same letter exists, it turns green before the typing gets there.
