88c53c455dd767e67efc37099e37e5e99b524cc5
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-10 03:51:07 +0000

    working on object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,25 @@
 /* exported defaults */
+
+// 2 for in loops, the first one to loop through source object
+// get the property
+// then do second loop of the target object and get property
+// if source[property] !== target[property],
+// then add it into the target object
+// if not, then move on
+
+function defaults(target, source) {
+  for (var keys in target) {
+    var propertyTarget = keys;
+    for (var property in source) {
+      var propertySource = property;
+      if (target[property] === {}) {
+        target = source;
+      } else if (propertySource !== propertyTarget) {
+        target[propertySource] = source[propertySource];
+        break;
+      } else {
+        break;
+      }
+    }
+  }
+}
