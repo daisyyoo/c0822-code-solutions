5b7cb504cd9fe5c01edeee67c25a4ec71bbcc293
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-19 22:00:09 +0000

    Completed javascript-constructors

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,23 @@
+var obj = new Object();
+console.log('value of object: ', obj);
+console.log('typeof object: ', typeof obj);
+
+var array = new Array();
+console.log('value of array: ', array);
+console.log('typeof array: ', typeof array);
+
+var newFunction = new Function();
+console.log('value of newFunction: ', newFunction);
+console.log('typeof newFunction: ', typeof newFunction);
+
+var string = new String();
+console.log('value of string: ', string);
+console.log('typeof string: ', typeof string);
+
+var number = new Number();
+console.log('value of number: ', number);
+console.log('typeof number: ', typeof number);
+
+var boolean = new Boolean();
+console.log('value of boolean: ', boolean);
+console.log('typeof boolean: ', typeof boolean);
