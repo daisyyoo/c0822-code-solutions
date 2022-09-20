145b110d8deba6cf7d9abd3aef88ac9e3012ee2d
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-20 19:33:56 +0000

    Almost fully functioning carousel, need to match bottom buttons with interval

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,9 +1,106 @@
-// var imageNum = 1;
-// var countdownID = null;
-// var $image = document.querySelector('img');
+var imageNum = 0;
+var countdownID = null;
+var $images = document.querySelectorAll('img');
 
-// function countdown() {
+function countdown() {
+  if (imageNum < 4) {
+    $images[imageNum].className = 'hidden';
+    imageNum++;
+    $images[imageNum].className = 'show';
+  } else if (imageNum === 4) {
+    $images[imageNum].className = 'hidden';
+    imageNum = 0;
+    $images[imageNum].className = 'show';
+  }
+}
 
-// }
+countdownID = setInterval(countdown, 3000);
 
-// countdownID = setInterval(countdown, 3000);
+var $chevronRight = document.querySelector('.chevron-right');
+var $chevronLeft = document.querySelector('.chevron-left');
+
+$chevronRight.addEventListener('click', clickRight);
+$chevronLeft.addEventListener('click', clickLeft);
+
+function clickRight() {
+  countdown();
+  clearInterval(countdownID);
+  countdownID = setInterval(countdown, 3000);
+}
+
+function clickLeft() {
+  if (imageNum > 0 && imageNum <= 4) {
+    $images[imageNum].className = 'hidden';
+    imageNum--;
+    $images[imageNum].className = 'show';
+  } else if (imageNum === 0) {
+    $images[imageNum].className = 'hidden';
+    imageNum = 4;
+    $images[imageNum].className = 'show';
+  }
+  clearInterval(countdownID);
+  countdownID = setInterval(countdown, 3000);
+}
+
+var $circle0 = document.querySelector('.circle-0');
+var $circle1 = document.querySelector('.circle-1');
+var $circle2 = document.querySelector('.circle-2');
+var $circle3 = document.querySelector('.circle-3');
+var $circle4 = document.querySelector('.circle-4');
+var $circlesAll = document.querySelectorAll('.image-nav > i');
+
+$circle0.addEventListener('click', goToImg0);
+$circle1.addEventListener('click', goToImg1);
+$circle2.addEventListener('click', goToImg2);
+$circle3.addEventListener('click', goToImg3);
+$circle4.addEventListener('click', goToImg4);
+
+function goToImg0() {
+  $images[imageNum].className = 'hidden';
+  $circlesAll[imageNum].className = 'fa-regular fa-circle';
+  imageNum = 0;
+  $images[imageNum].className = 'show';
+  $circlesAll[imageNum].className = 'fa-solid fa-circle';
+  clearInterval(countdownID);
+  countdownID = setInterval(countdown, 3000);
+}
+
+function goToImg1() {
+  $images[imageNum].className = 'hidden';
+  $circlesAll[imageNum].className = 'fa-regular fa-circle';
+  imageNum = 1;
+  $images[imageNum].className = 'show';
+  $circlesAll[imageNum].className = 'fa-solid fa-circle';
+  clearInterval(countdownID);
+  countdownID = setInterval(countdown, 3000);
+}
+
+function goToImg2() {
+  $images[imageNum].className = 'hidden';
+  $circlesAll[imageNum].className = 'fa-regular fa-circle';
+  imageNum = 2;
+  $images[imageNum].className = 'show';
+  $circlesAll[imageNum].className = 'fa-solid fa-circle';
+  clearInterval(countdownID);
+  countdownID = setInterval(countdown, 3000);
+}
+
+function goToImg3() {
+  $images[imageNum].className = 'hidden';
+  $circlesAll[imageNum].className = 'fa-regular fa-circle';
+  imageNum = 3;
+  $images[imageNum].className = 'show';
+  $circlesAll[imageNum].className = 'fa-solid fa-circle';
+  clearInterval(countdownID);
+  countdownID = setInterval(countdown, 3000);
+}
+
+function goToImg4() {
+  $images[imageNum].className = 'hidden';
+  $circlesAll[imageNum].className = 'fa-regular fa-circle';
+  imageNum = 4;
+  $images[imageNum].className = 'show';
+  $circlesAll[imageNum].className = 'fa-solid fa-circle';
+  clearInterval(countdownID);
+  countdownID = setInterval(countdown, 3000);
+}
