e2671560505fba7fae416d406d923f482d997fac
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-20 18:21:05 +0000

    HTML and CSS skeleton finished

---
+++ javascript-carousel/main.js
@@ -0,0 +1,9 @@
+// var imageNum = 1;
+// var countdownID = null;
+// var $image = document.querySelector('img');
+
+// function countdown() {
+
+// }
+
+// countdownID = setInterval(countdown, 3000);
