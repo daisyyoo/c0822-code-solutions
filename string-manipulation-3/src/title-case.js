69880a7ca543096cfbe99430a702490e3600e787
Daisy Yoo <daisyhyoo@gmail.com>
2022-10-03 21:02:11 +0000

    Completed string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -17,15 +17,13 @@ function titleCase(title) {
     for (var j = 0; j < specialArray.length; j++) {
       if (specialArray.indexOf(titleArray[i]) === -1) {
         titleArray[i] = titleArray[i][0].toUpperCase() + titleArray[i].slice(1);
+      } else if (titleArray[i] !== titleArray[0] && titleArray[i - 1].includes('JavaScript:')) {
+        titleArray[i] = titleArray[i][0].toUpperCase() + titleArray[i].slice(1);
       }
-      // else if (titleArray[i] !== titleArray[0] && titleArray[i - 1].includes('javascript:')) {
-      //   titleArray[i] = titleArray[i][0].toUpperCase() + titleArray[i].slice(1);
-      // }
     } if (titleArray[i] === 'Api') {
       titleArray[i] = 'API';
     } else if (titleArray[i].includes('Javascript')) {
       titleArray[i] = titleArray[i].replace('Javascript', 'JavaScript');
-      // titleArray[i + 1] = titleArray[i + 1][0].toUpperCase() + titleArray[i + 1].slice(1);
     } else if (titleArray[i].includes('-')) {
       var hyphenWordSplit = titleArray[i].split('-');
       hyphenWordSplit[0] = hyphenWordSplit[0][0].toUpperCase() + hyphenWordSplit[0].slice(1);
@@ -40,21 +38,3 @@ function titleCase(title) {
   titleString = titleString[0].toUpperCase() + titleString.slice(1);
   return titleString;
 }
-
-// titleLowerCase = titleLowerCase.replace(titleLowerCase[0], titleLowerCase[0].toUpperCase());
-// for (var i = 0; i < titleLowerCase.length; i++) {
-//   if (titleLowerCase.includes('api')) {
-//     titleLowerCase = titleLowerCase.replace('api', 'API');
-//   } else if (titleLowerCase.includes('javascript')) {
-//     titleLowerCase = titleLowerCase.replace('javascript', 'JavaScript');
-//   } else if (titleLowerCase[i] === '-') {
-//     titleLowerCase = titleLowerCase.charAt(i + 1).toUpperCase() + titleLowerCase.slice(i + 2);
-//   } else if (titleLowerCase[i] === ' ') {
-//     titleLowerCase = titleLowerCase.charAt(i + 1).toUpperCase() + titleLowerCase.slice(i + 2);
-//   }
-// for (var j = 0; j < specialArray.length; j++) {
-//   var capSpecialWord = specialArray[j].replace(specialArray[j][0], specialArray[j][0].toUpperCase());
-//   if (titleLowerCase.includes(capSpecialWord)) {
-//     titleLowerCase.replace(capSpecialWord, capSpecialWord.replace(capSpecialWord[j][0], capSpecialWord[j].toLowerCase()));
-//   }
-// }
