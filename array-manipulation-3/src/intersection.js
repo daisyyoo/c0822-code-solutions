56acafbc7fdc4eae699c1cb7c741edd4c6256ce8
Daisy Yoo <daisyhyoo@gmail.com>
2022-10-03 22:39:54 +0000

    Finished intersection function, 6th problem

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,17 @@
 /* exported intersection */
+
+// make a new array
+// use a for loop to loop through the first array
+// use indexOf method to see if it equals to 0 or larger (meaning it's present in the other array)
+// then push into new array
+// return new array
+
+function intersection(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (second.indexOf(first[i]) >= 0) {
+      newArray.push(first[i]);
+    }
+  }
+  return newArray;
+}
