568a309d2f00d7a2ab722d032cededed08e87aee
Daisy Yoo <daisyhyoo@gmail.com>
2022-10-13 16:56:49 +0000

    create function working

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -4,15 +4,19 @@ const fs = require('fs');
 if (process.argv[2] === 'read') {
   for (let i = 1; i < dataObject.nextId; i++) {
     fs.readFile('./data.json', 'utf8', () => {
-      console.log(`${i}: ${dataObject.notes[i]}`);
+      const fileContent = `${i}: ${dataObject.notes[i]}`;
+      console.log(fileContent);
     });
   }
 } else if (process.argv[2] === 'create') {
-
-  console.log(process.argv[3]);
-  console.log(dataObject.notes);
-  console.log(dataObject.notes[dataObject.nextId]);
-  // const newEntry = dataObject.notes[`${dataObject.nextId.toString()}`: ${process.argv[3]}`];
-  // JSON.parse(newEntry);
-  // dataObject.nextId++;
+  const newEntryNum = dataObject.nextId.toString();
+  const newEntryContent = process.argv[3];
+  dataObject.notes[newEntryNum] = newEntryContent;
+  dataObject.nextId++;
+  fs.writeFile('./data.json', JSON.stringify(dataObject, null, 2), err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
 }
