59acd078d8ef116d8f91e03fcfa4730953d0c557
Daisy Yoo <daisyhyoo@gmail.com>
2022-10-13 02:47:06 +0000

    Working on create function

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -7,4 +7,12 @@ if (process.argv[2] === 'read') {
       console.log(`${i}: ${dataObject.notes[i]}`);
     });
   }
+} else if (process.argv[2] === 'create') {
+
+  console.log(process.argv[3]);
+  console.log(dataObject.notes);
+  console.log(dataObject.notes[dataObject.nextId]);
+  // const newEntry = dataObject.notes[`${dataObject.nextId.toString()}`: ${process.argv[3]}`];
+  // JSON.parse(newEntry);
+  // dataObject.nextId++;
 }
