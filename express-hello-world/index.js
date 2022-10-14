d44f036020592e76ba2629fb0e07004791c02c90
Daisy Yoo <daisyhyoo@gmail.com>
2022-10-14 03:52:53 +0000

    Completed express-hello-world

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.use((req, res) => {
-  // req.method('GET');
   res.send('Hello World!');
 });
 
 app.listen(3000, () => {
-  // console.log('The server is listening');
+  // eslint-disable-next-line no-console
+  console.log('The server is listening');
 });
