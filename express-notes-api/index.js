337fe66cdc4c279ac07fc32567e1b18baf3f5ace
Daisy Yoo <daisyhyoo@gmail.com>
2022-10-19 07:53:16 +0000

    Revised code for express-notes-api

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -5,45 +5,29 @@ const dataObject = require('./data.json');
 const fs = require('fs');
 
 app.get('/api/notes', (req, res) => {
-  fs.readFile('./data.json', 'utf8', err => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    } else {
-      res.status(200);
-      const dataArray = [];
-      for (const key in dataObject.notes) {
-        dataArray.push(dataObject.notes[key]);
-      }
-      res.send(dataArray);
-    }
-  });
+  res.status(200);
+  const dataArray = [];
+  for (const key in dataObject.notes) {
+    dataArray.push(dataObject.notes[key]);
+  }
+  res.send(dataArray);
 });
 
 app.get('/api/notes/:id', (req, res, next) => {
   const idNum = req.params.id;
   const errId = { error: '' };
-  fs.readFile('./data.json', 'utf8', err => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    for (const key in dataObject.notes) {
-      const id = key;
-      if (!Number(idNum) || Number(idNum) < 0) {
-        res.status(400);
-        errId.error = 'id must be a positive integer';
-      } else if (id !== idNum) {
-        res.status(404);
-        errId.error = `cannot find note with id ${idNum}`;
-      } else if (id === idNum) {
-        res.status(200);
-        res.send(dataObject.notes[idNum]);
-        return next();
-      }
-    }
-    res.send(errId);
-  });
+  if (!Number(idNum) || Number(idNum) < 0) {
+    res.status(400);
+    errId.error = 'id must be a positive integer';
+  } else if (!(idNum in dataObject.notes)) {
+    res.status(404);
+    errId.error = `cannot find note with id ${idNum}`;
+  } else if (idNum in dataObject.notes) {
+    res.status(200);
+    res.send(dataObject.notes[idNum]);
+    return next();
+  }
+  res.send(errId);
 });
 
 const jsonMiddleware = express.json();
@@ -57,15 +41,13 @@ app.post('/api/notes', (req, res) => {
     content: newContent.content
   };
   const id = dataObject.nextId;
-  for (const key in newContent) {
-    if (key !== 'content') {
-      res.status(400);
-      errId.error = 'content is a required field';
-      res.send(errId);
-    } else if (key === 'content') {
-      dataObject.notes[dataObject.nextId] = newNote;
-      dataObject.nextId++;
-    }
+  if (!('content' in newContent)) {
+    res.status(400);
+    errId.error = 'content is a required field';
+    res.send(errId);
+  } else if ('content' in newContent) {
+    dataObject.notes[dataObject.nextId] = newNote;
+    dataObject.nextId++;
   }
   fs.writeFile('./data.json', JSON.stringify(dataObject, null, 2), err => {
     if (err) {
@@ -82,19 +64,17 @@ app.post('/api/notes', (req, res) => {
 app.delete('/api/notes/:id', (req, res, next) => {
   const idNum = req.params.id;
   const errId = { error: '' };
-  for (const key in dataObject.notes) {
-    if (!Number(idNum) || Number(idNum) < 0) {
-      res.status(400);
-      errId.error = 'id must be a positive integer';
-    } else if (key !== idNum) {
-      res.status(404);
-      errId.error = `cannot find note with id ${idNum}`;
-    } else if (key === idNum) {
-      res.status(204);
-      delete dataObject.notes[idNum];
-      res.send(dataObject.notes[idNum]);
-      return next();
-    }
+  if (!Number(idNum) || Number(idNum) < 0) {
+    res.status(400);
+    errId.error = 'id must be a positive integer';
+  } else if (!(idNum in dataObject.notes)) {
+    res.status(404);
+    errId.error = `cannot find note with id ${idNum}`;
+  } else if (idNum in dataObject.notes) {
+    res.status(204);
+    delete dataObject.notes[idNum];
+    res.send(dataObject.notes[idNum]);
+    return next();
   }
   fs.writeFile('./data.json', JSON.stringify(dataObject, null, 2), err => {
     if (err) {
@@ -110,26 +90,22 @@ app.put('/api/notes/:id', (req, res, next) => {
   const idNum = req.params.id;
   const newContent = req.body;
   const errId = { error: '' };
-  for (const key in dataObject.notes) {
-    for (const property in newContent) {
-      if (!Number(idNum) || Number(idNum) < 0) {
-        res.status(400);
-        errId.error = 'id must be a positive integer';
-      } else if (property !== 'content') {
-        res.status(400);
-        errId.error = 'content is a required field';
-      } else if (key !== idNum) {
-        res.status(404);
-        errId.error = `cannot find note with id ${idNum}`;
-      } else if (key === idNum && property === 'content') {
-        res.status(200);
-        dataObject.notes[key] = newContent;
-        dataObject.notes[key].id = Number(key);
-        res.json(dataObject.notes[key]);
-        res.send();
-        return next();
-      }
-    }
+  if (!Number(idNum) || Number(idNum) < 0) {
+    res.status(400);
+    errId.error = 'id must be a positive integer';
+  } else if (!('content' in newContent)) {
+    res.status(400);
+    errId.error = 'content is a required field';
+  } else if (!(idNum in dataObject.notes)) {
+    res.status(404);
+    errId.error = `cannot find note with id ${idNum}`;
+  } else if (idNum in dataObject.notes && 'content' in newContent) {
+    res.status(200);
+    dataObject.notes[idNum] = newContent;
+    dataObject.notes[idNum].id = Number(idNum);
+    res.json(dataObject.notes[idNum]);
+    res.send();
+    return next();
   }
   fs.writeFile('./data.json', JSON.stringify(dataObject, null, 2), err => {
     if (err) {
