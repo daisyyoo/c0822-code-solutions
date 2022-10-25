870ccd430a73d67e9e8990259bfc5afb4474c1d6
Daisy Yoo <daisyhyoo@gmail.com>
2022-10-25 21:01:53 +0000

    Completed react-toggle-switch-component

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ToggleButton from './toggle-switch';
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(<ToggleButton />);
