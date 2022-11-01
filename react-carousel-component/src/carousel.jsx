4527cb1aeb9a65ffe4d6730ec0c32c3808e94a40
Daisy Yoo <daisyhyoo@gmail.com>
2022-11-01 06:16:59 +0000

    Skeleton done, working on React elements

---
+++ react-carousel-component/src/carousel.jsx
@@ -0,0 +1,29 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// class Carousel extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { currentIndex: null };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   render() {
+//     return (
+//       <div className="container">
+
+//       </div>
+//     );
+//   }
+// }
+
+// function renderImages(img) {
+//   this.props.image.map((image, index) => {
+//     const handleClick = () => {
+//       this.setOpenImage(index);
+//     };
+//   return (
+//     <img key={index} className="show" src={image}></img>
+//   );
+//   }
+// }
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -6,16 +6,27 @@ class Carousel extends React.Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="left-arrow">
-
+          <div className="left-arrow col-3">
+            <i className="fa-sharp fa-solid fa-circle-arrow-left"></i>
           </div>
-          <div className="image-container">
-            <div className="">
-
+          <div className="image-container col-3">
+            <div className="images">
+              <img className="show" src="../images/001.png"></img>
+              <img className="hide" src="../images/004.png"></img>
+              <img className="hide" src="../images/007.png"></img>
+              <img className="hide" src="../images/025.png"></img>
+              <img className="hide" src="../images/039.png"></img>
+            </div>
+            <div className="bottom-nav">
+              <i className="fa-solid fa-circle"></i>
+              <i className="fa-regular fa-circle"></i>
+              <i className="fa-regular fa-circle"></i>
+              <i className="fa-regular fa-circle"></i>
+              <i className="fa-regular fa-circle"></i>
             </div>
           </div>
-          <div className="right-arrow">
-
+          <div className="right-arrow col-3">
+            <i className="fa-sharp fa-solid fa-circle-arrow-right"></i>
           </div>
         </div>
       </div>
