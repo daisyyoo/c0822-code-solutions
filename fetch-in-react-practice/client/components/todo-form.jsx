72cf844772391cb96e354caea899cec6f488a21d
Daisy Yoo <daisyhyoo@gmail.com>
2023-03-02 21:07:34 +0000

    Found mistake of npm installing package in wrong package.json and fixed rendering of page by uninstalling package in parent package.json and installing in child package.json

--- fetch-in-react-practice/client/components/todo-form.jsx
+++ fetch-in-react-practice/client/components/todo-form.jsx
@@ -1,50 +1,59 @@
-// import React, { useState } from 'react';
-// import { useForm } from 'react-hook-form';
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 
-// function TodoForm() {
-//   const [task, setTask] = useState({ task: '' });
-//   const { register, handleSubmit, formState: { errors } } = useForm();
+export default function TodoForm(props) {
+  // const [task, setTask] = useState({ task: '' });
+  const { register, handleSubmit, reset, formState, formState: { errors } } = useForm({
+    defaultValues: {
+      task: ''
+    }
+  });
 
-//   // constructor(props) {
-//   //   super(props);
-//   //   this.state = {
-//   //     task: ''
-//   //   };
-//   //   this.handleChange = this.handleChange.bind(this);
-//   //   this.handleSubmit = this.handleSubmit.bind(this);
-//   // }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     task: ''
+  //   };
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
 
-//   // const handleChange = (event) {
-//   //   this.setState({
-//   //     task: event.target.value
-//   //   });
-//   // }
+  // const handleChange = (event) {
+  //   this.setState({
+  //     task: event.target.value
+  //   });
+  // }
 
-//   // handleSubmit(event) {
-//   //   event.preventDefault();
-//   //   const newTodo = {
-//   //     task: this.state.task,
-//   //     isCompleted: false
-//   //   };
-//   //   this.props.onSubmit(newTodo);
-//   //   this.setState({ task: '' });
-//   // }
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   const newTodo = {
+  //     task: this.state.task,
+  //     isCompleted: false
+  //   };
+  //   this.props.onSubmit(newTodo);
+  //   this.setState({ task: '' });
+  // }
 
-//   return (
-//     <form className="input-group mb-4 shadow-sm" onSubmit={handleSubmit(onSubmit)}>
-//       <input
-//         required
-//         autoFocus
-//         type="text"
-//         value={task}
-//         className="form-control"
-//         placeholder="What to do?"
-//         {...register(task)}/>
-//       <div className="input-group-append">
-//         <button type="submit" className="btn btn-primary">Add Todo</button>
-//       </div>
-//     </form>
-//   );
-// }
+  const onSubmit = data => {
+    // console.log(data);
+  };
 
-// export default TodoForm;
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset();
+    }
+  }, [formState.isSubmitSuccessful, reset]);
+
+  return (
+    <form className="input-group mb-4 shadow-sm" onSubmit={handleSubmit(onSubmit)}>
+      <input
+        className="form-control"
+        placeholder="What to do?"
+        {...register('task', { required: true, minLength: 2 })}/>
+        {errors.task && <p>This is required.</p>}
+      <div className="input-group-append">
+        <button type="submit" className="btn btn-primary">Add Todo</button>
+      </div>
+    </form>
+  );
+}
